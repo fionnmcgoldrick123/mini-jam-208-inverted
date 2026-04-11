f892c31558e7fce5f7e38e49005e69efe9dfdc9f
fionn mcgoldrick <fionnmc123@gmail.com>
2026-04-11 01:31:23 +0100

    Add enemy death animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,16 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 3;
     private int currentHealth;
+    private Animator animator;
+    private Collider2D enemyCollider;
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+        enemyCollider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public bool TakeDamage(int amount)
@@ -25,6 +31,31 @@ public class Enemy : MonoBehaviour
 
     private void Die()
     {
+        // Disable collision immediately
+        if (enemyCollider != null)
+            enemyCollider.enabled = false;
+
+        // Play death animation if animator exists
+        if (animator != null)
+        {
+            animator.SetTrigger("Hit");
+        }
+        else
+        {
+            // No animator, hide and destroy immediately
+            if (spriteRenderer != null)
+                spriteRenderer.enabled = false;
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnDeathAnimationComplete()
+    {
+        // Disable sprite renderer
+        if (spriteRenderer != null)
+            spriteRenderer.enabled = false;
+        
+        // Destroy the game object
         Destroy(gameObject);
     }
 }
