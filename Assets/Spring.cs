6aaae08f2a43b6006986a2f4559f2fa80ced1737
fionn mcgoldrick <fionnmc123@gmail.com>
2026-04-11 15:50:36 +0100

    Add spring functionality and animation

--- Assets/Spring.cs
+++ Assets/Spring.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class Spring : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float bounceForce = 10f;
+    [SerializeField] private float animationResetDelay = 0.12f;
+
+    private Animator animator;
+    private Coroutine resetRoutine;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            if (animator != null)
+            {
+                animator.ResetTrigger("Spring");
+                animator.SetTrigger("Spring");
+
+                if (resetRoutine != null)
+                    StopCoroutine(resetRoutine);
+
+                resetRoutine = StartCoroutine(ResetAnimationAfterDelay());
+            }
+
+            Rigidbody2D rb = collision.attachedRigidbody;
+            if (rb != null)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+
+                rb.AddForce(new Vector2(0, bounceForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private System.Collections.IEnumerator ResetAnimationAfterDelay()
+    {
+        yield return new WaitForSeconds(animationResetDelay);
+
+        if (animator != null)
+        {
+            animator.Rebind();
+            animator.Update(0f);
+        }
+
+        resetRoutine = null;
     }
 }
