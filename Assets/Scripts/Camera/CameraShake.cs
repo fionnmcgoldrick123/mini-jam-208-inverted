cea9466444fee8cb62fc26910d796ba977b024de
fionn mcgoldrick <fionnmc123@gmail.com>
2026-04-15 11:24:56 +0100

    Redesigned and imported tree decoration sprites

--- Assets/Scripts/Camera/CameraShake.cs
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -7,16 +7,10 @@ public class CameraShake : MonoBehaviour
     [SerializeField] private float shakeDuration = 0.1f;
     [SerializeField] private float shakeFrequency = 35f;
 
-    private Vector3 originalPosition;
     private float shakeTimer;
     private float currentDuration;
     private float seedX;
     private float seedY;
-
-    private void Start()
-    {
-        originalPosition = transform.localPosition;
-    }
 
     private void LateUpdate()
     {
@@ -32,18 +26,14 @@ public class CameraShake : MonoBehaviour
             float x = (Mathf.PerlinNoise(seedX, time) - 0.5f) * 2f * shakeAmount * envelope;
             float y = (Mathf.PerlinNoise(seedY, time) - 0.5f) * 2f * shakeAmount * envelope;
 
-            transform.localPosition = originalPosition + new Vector3(x, y, 0);
-
-            if (shakeTimer <= 0)
-            {
-                transform.localPosition = originalPosition;
-            }
+            Vector3 shakeOffset = new Vector3(x, y, 0);
+            transform.position += shakeOffset;
         }
     }
 
     public void Shake(float intensity = 1f, float duration = -1f)
     {
-        originalPosition = transform.localPosition;
+        Debug.Log($"Camera shake called: intensity={intensity}, duration={duration}");
         shakeAmount = intensity;
         currentDuration = duration < 0 ? shakeDuration : duration;
         shakeTimer = currentDuration;
@@ -51,3 +41,4 @@ public class CameraShake : MonoBehaviour
         seedY = Random.Range(0f, 1000f);
     }
 }
+
